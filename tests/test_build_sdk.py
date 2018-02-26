050932f30809dfcb0bbf11e37c8848f45153140d
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-02-26 09:04:59 -0800

    Remove deprecated code

--- tests/test_build_sdk.py
+++
@@ -1,5 +0,0 @@
-from swaggertosdk.build_sdk import *
-
-def test_guess_autorest_options():
-    assert guess_service_info_from_path("specification/compute/resource-manager/readme.md") == {"rp_name": "compute", "is_arm": True}
-    assert guess_service_info_from_path("specification/servicefabric/data-plane/readme.md") == {"rp_name": "servicefabric", "is_arm": False}
