ab8484f35a79017c4816965ef2b80c6fd170faac
Jianghao Lu <jianghaolu@users.noreply.github.com>
2018-07-05 09:01:58 -0700

    Modify java repo (#55)

--- swaggertosdk/SwaggerToSdkCore.py
+++ swaggertosdk/SwaggerToSdkCore.py
@@ -47,7 +47,7 @@ def get_repo_tag_meta(meta_conf):
     if "ruby" in meta_conf["autorest_options"]:
         return "azure-sdk-for-ruby"
     if "java" in meta_conf["autorest_options"]:
-        return "azure-libraries-for-java"
+        return "azure-sdk-for-java"
     if "nodejs" in meta_conf["autorest_options"]:
         return "azure-sdk-for-node"
     raise ValueError("No repotag found or infered")
